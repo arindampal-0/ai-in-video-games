98e176b0e18f474fdd178ba86aa50de76f9e33a0
Arindam Pal <arindampal2000@gmail.com>
2024-11-05 09:49:46 +0530

    fixed collision and y-sorting

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,8 +1,26 @@
 extends CharacterBody2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 @export var player: CharacterBody2D
 @export var min_distance: float = 50.0
 @export var speed: float = 50.0
+
+enum SlimeState {
+	IDLE = 0,
+	WALK,
+	CHASE
+}
+
+enum Direction {
+	LEFT = 0,
+	RIGHT,
+	BACK,
+	FRONT,
+}
+
+var state: SlimeState = SlimeState.IDLE
+var dir: Direction = Direction.FRONT
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,8 +31,44 @@ func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
 	if global_position.distance_to(player.global_position) > min_distance:
 		velocity = direction * speed
+		
+		if direction.x < 0:
+			dir = Direction.LEFT
+		elif direction.x > 0:
+			dir = Direction.RIGHT
+
+		if direction.y < 0:
+			dir = Direction.BACK
+		elif direction.y > 0:
+			dir = Direction.FRONT
+		
+		state = SlimeState.CHASE
 	else:
 		velocity = Vector2.ZERO
+		state = SlimeState.IDLE
+		
+	if state == SlimeState.IDLE:
+		if dir == Direction.BACK:
+			animated_sprite.play("idle_back")
+		elif dir == Direction.LEFT:
+			animated_sprite.play("idle_right")
+			animated_sprite.flip_h = true
+		elif dir == Direction.RIGHT:
+			animated_sprite.play("idle_right")
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.play("idle_front")
+	elif state == SlimeState.CHASE:
+		if dir == Direction.BACK:
+			animated_sprite.play("walk_back")
+		elif dir == Direction.LEFT:
+			animated_sprite.play("walk_right")
+			animated_sprite.flip_h = true
+		elif dir == Direction.RIGHT:
+			animated_sprite.play("walk_right")
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.play("walk_front")
 	
 	move_and_slide()
 	
